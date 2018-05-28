08cb26e58163fd056b60f8b6e2af48aa629c2548
Avinash Kashyap <iavinashkashyap@gmail.com>
2018-05-29 04:02:32 +0530

    first commit

---
+++ FrameworkSample/FrameworkSample.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkSample.h
+//  FrameworkSample
+//
+//  Created by Avinash Kashyap on 29/05/18.
+//  Copyright © 2018 Avinash Kashyap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkSample.
+FOUNDATION_EXPORT double FrameworkSampleVersionNumber;
+
+//! Project version string for FrameworkSample.
+FOUNDATION_EXPORT const unsigned char FrameworkSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkSample/PublicHeader.h>
+
+
